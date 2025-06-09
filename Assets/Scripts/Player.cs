7648eef0896546856023e54f43606eb5c4ef860d
Antony Sajan <antonysajant@gmail.com>
2025-06-09 21:42:32 +0530

    DO NOT PLAY THE GAME UNLESS YOU GOT MORE THAN 16 GIGS RAM

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,18 @@ public class Player : MonoBehaviour
     Animator anim;
     bool isIdle = true;
     Rigidbody2D rb;
-    [SerializeField] Vector3 newPos;
+    public Vector3 newPos;
     [SerializeField] Vector3 current;
     [SerializeField] GameObject checkUp;
     [SerializeField] GameObject checkDown;
     [SerializeField] GameObject checkLeft;
     [SerializeField] GameObject checkRight;
     DragController dg;
-    [SerializeField] int count = 0;
     [SerializeField] bool dgmove;
+    [SerializeField] GameObject waypoint;
+    [SerializeField] float yOffset;
+    [SerializeField] float xOffset;
+    public bool wpcreated = false;
 
     private void Awake()
     {
@@ -43,12 +46,6 @@ public class Player : MonoBehaviour
             if (Vector3.Distance(transform.position, newPos) < 0.02f)
             {
                 goIdle();
-                if (count == 2)
-                {
-                    count = 0;
-                    dg.move = false;
-                    Debug.Log("Move FALSE");
-                }
             }
         }
             
@@ -64,10 +61,9 @@ public class Player : MonoBehaviour
         anim.SetBool("runD", false);
         isIdle = true;
         anim.SetBool("idleD", true);
-        count++;
     }
 
-    void startMove() // sets isIdle and animation to false
+    public void startMove() // sets isIdle and animation to false
     {
         isIdle = false;
         anim.SetBool("idleD", false);
@@ -97,49 +93,53 @@ public class Player : MonoBehaviour
 
         if (!isIdle) return;
 
-        if (count == 2) return;
+        if (wpcreated) return;
 
-        //Vector3 x = Vector3.zero ;
+        GameObject waypoints = new GameObject("Waypoints");
+
+        Vector3 x;
+        Vector3 xoff = new Vector3(xOffset, 0, 0);
+        Vector3 yoff = new Vector3(0, yOffset, 0);
         if (Physics2D.OverlapPoint(checkUp.transform.position) == null || IsGateAt(checkUp.transform.position))
         {
-            //do { x = x + Vector3.up; } while (Physics2D.OverlapPoint(checkUp.transform.position+x)==null || IsGateAt(checkUp.transform.position+x));
-            Debug.Log("Moving UP");
-            anim.SetBool("runU", true);
-            newPos = checkUp.transform.position/* + x - 2*Vector3.down */;
-            startMove();
+            x = Vector3.zero;
+            do
+            {
+                GameObject wp=Instantiate(waypoint,checkUp.transform.position + x-yoff, Quaternion.identity);
+                wp.transform.parent=waypoints.transform;
+                x = x + Vector3.up;
+            } while (Physics2D.OverlapPoint(checkUp.transform.position + x) == null || IsGateAt(checkUp.transform.position + x)) ;
         }
-        else
+        if (Physics2D.OverlapPoint(checkLeft.transform.position) == null)
         {
-            if (Physics2D.OverlapPoint(checkLeft.transform.position) == null)
+            x = Vector3.zero;
+            do
             {
-                //do { x = x + Vector3.left; } while (Physics2D.OverlapPoint(checkLeft.transform.position + x) == null || IsGateAt(checkLeft.transform.position + x));
-                Debug.Log("Moving LEFT");
-                anim.SetBool("runL", true);
-                newPos = checkLeft.transform.position/* + x - 2*Vector3.right */;
-                startMove();
-            }
-            else
-            {
-                if (Physics2D.OverlapPoint(checkRight.transform.position) == null)
-                {
-                    //do { x = x + Vector3.right; } while (Physics2D.OverlapPoint(checkRight.transform.position + x) == null || IsGateAt(checkRight.transform.position + x));
-                    Debug.Log("Moving RIGHT");
-                    anim.SetBool("runR", true);
-                    newPos = checkRight.transform.position/* + x - 2*Vector3.left */;
-                    startMove();
-                }
-                else
-                {
-                    if (Physics2D.OverlapPoint(checkDown.transform.position) == null)
-                    {
-                        //do { x = x + Vector3.down; } while (Physics2D.OverlapPoint(checkDown.transform.position + x) == null || IsGateAt(checkDown.transform.position + x));
-                        Debug.Log("Moving DOWN");
-                        anim.SetBool("runD", true);
-                        newPos = checkDown.transform.position/* + x - 2 * Vector3.up */;
-                        startMove();
-                    }
-                }
-            }
+                GameObject wp = Instantiate(waypoint, checkLeft.transform.position + x +xoff-yoff, Quaternion.identity);
+                wp.transform.parent = waypoints.transform;
+                x = x + Vector3.left;
+            } while (Physics2D.OverlapPoint(checkLeft.transform.position + x) == null);
         }
+        if (Physics2D.OverlapPoint(checkRight.transform.position) == null)
+        {
+            x = Vector3.zero;
+            do
+            {
+                GameObject wp = Instantiate(waypoint, checkRight.transform.position + x -xoff-yoff, Quaternion.identity);
+                wp.transform.parent = waypoints.transform;
+                x = x + Vector3.right;
+            } while (Physics2D.OverlapPoint(checkRight.transform.position + x) == null);
+        }
+        if (Physics2D.OverlapPoint(checkDown.transform.position) == null || !IsGateAt(checkDown.transform.position))
+        {
+            x = Vector3.zero;
+            do
+            {
+                GameObject wp = Instantiate(waypoint, checkDown.transform.position + x + yoff, Quaternion.identity);
+                wp.transform.parent = waypoints.transform;
+                x = x + Vector3.down;
+            } while (Physics2D.OverlapPoint(checkDown.transform.position + x) == null || !IsGateAt(checkDown.transform.position + x));
+        }
+        wpcreated = true;
     }
 }
