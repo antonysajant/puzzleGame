1d1ac9bba272dedfd72031d212af82853003b577
Antony Sajan <antonysajant@gmail.com>
2025-06-20 23:26:38 +0530

    overhead ui added

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -2,14 +2,10 @@ using UnityEngine;
 
 public class Treasure : MonoBehaviour
 {
-    [SerializeField]GameObject closed;
-    [SerializeField]GameObject open;
-
-    void Awake()
-    {
-        //closed = transform.GetChild(0).gameObject;
-        //open=transform.GetChild(1).gameObject;
-    }
+    [SerializeField] GameObject closed;
+    [SerializeField] GameObject open;
+    [SerializeField] GameObject ui;
+    bool opened = false;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,6 +13,18 @@ public class Treasure : MonoBehaviour
         { 
             closed.SetActive(false);
             open.SetActive(true);
+            opened = true;
         }
     }
+
+
+    void Update()
+    { 
+        Collider2D hit = Physics2D.OverlapPoint(transform.position);
+        if (hit != null && hit.transform.tag == "Block" && !opened)
+            ui.SetActive(true);
+        else
+            ui.SetActive(false);
+    }
+
 }
